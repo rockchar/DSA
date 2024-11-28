8b1f5410700c83c7cbeee7fce8d523031ca1040c
Rohit Kumar <rohitsinha050481@gmail.com>
2024-11-28 09:32:08 +0530

    Place holders and master project

---
+++ SearchAlgorithms/BinarySearch/BinarySearch/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BinarySearch
+//
+//  Created by Rohit Kumar on 28/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Binary Search!\n";
+    return 0;
+}
---
+++ SearchAlgorithms/LinearSearch/LinearSearch/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LinearSearch
+//
+//  Created by Rohit Kumar on 28/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Linear Search \n";
+    return 0;
+}
